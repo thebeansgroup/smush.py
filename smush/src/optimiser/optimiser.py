79dcda8381fcd233ac6738c9dca791550cde867b
al <al@dev1.beans>
2010-08-11 17:26:36 +0100

    Updated the README

--- smush/src/optimiser/optimiser.py
+++ smush/src/optimiser/optimiser.py
@@ -54,7 +54,11 @@ class Optimiser:
         # if the image was optimised (output is smaller than input), overwrite the input file with the output
         # file.
         if (output_size < input_size):
-            shutil.copy(output, input)
+            try:
+                shutil.copyfile(output, input)
+            except IOError:
+                print "Unable to copy %s to %s: %s" % (output, input, IOError)
+                sys.exit(1)
         
         # delete the output file
         os.unlink(output)
@@ -77,7 +81,6 @@ class Optimiser:
         Calls the 'optimise_image' method on the object. Tests the 'optimised' file size. If the
         generated file is larger than the original file, discard it, otherwise discard the input file.
         """
-
         # make sure the input image is acceptable for this optimiser
         if not self.is_acceptable_image(self.input):
             return
