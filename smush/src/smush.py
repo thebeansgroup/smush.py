ed631ed37c2688505f14086610f37190ff2407d7
al <al@dev1.beans>
2010-08-23 11:31:16 +0100

    Can now optimise directories

--- smush/src/smush.py
+++ smush/src/smush.py
@@ -1,3 +1,8 @@
+#!/usr/local/bin/python2.7
+import sys
+import os
+import os.path
+import getopt
 from optimiser.formats.png import OptimisePNG
 from optimiser.formats.jpg import OptimiseJPG
 from optimiser.formats.gif import OptimiseGIF
@@ -7,19 +12,124 @@ __date__ ="$Aug 11, 2010 12:21:32 PM$"
 
 # there should be an option to keep or strip meta data (e.g. exif data) from jpegs
 
+class Smush():
+    def __init__(self):
+        self.optimisers = { 'PNG': OptimisePNG(),
+            'JPG': OptimiseJPG(),
+            'JPEG': OptimiseJPG(),
+            'GIF': OptimiseGIF()
+        }
+
+        self.__files_scanned = 0
+
+    def __smush(self, file):
+        """
+        Optimises a file
+        """
+        (basename, extension) = os.path.splitext(file)
+        key = extension.lstrip('.').upper()
+
+        if key in self.optimisers:
+            print "optimising file ", file
+
+            self.__files_scanned += 1
+
+            self.optimisers[key].set_input(file)
+            self.optimisers[key].optimise()
+
+
+    def process(self, dir, recursive):
+        """
+        Iterates through the input directory optimising files
+        """
+        if recursive:
+            self.__walk(dir, self.__smush)
+        else:
+            dir = os.path.abspath(dir)
+            for file in os.listdir(dir):
+                self.__smush(os.path.join(dir, file))
+
+
+    def __walk(self, dir, callback):
+        """ Walks a directory, and executes a callback on each file """
+        dir = os.path.abspath(dir)
+
+        print "walking ", dir
+
+        for file in os.listdir(dir):
+            nfile = os.path.join(dir, file)
+            callback(nfile)
+
+            if os.path.isdir(nfile):
+                self.walk(nfile, callback)
+
+    def stats(self):
+        print "%d files scanned" % (self.__files_scanned)
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hrq", ["help", "recursive", "quiet"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    recursive = False
+    quiet = False
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-r", "--recursive"):
+            recursive = True
+        elif opt in ("-q", "--quiet"):
+            quiet = True
+        else:
+            # unsupported option given
+            usage()
+            sys.exit(2)
+
+    smush = Smush()
+
+    for arg in args:
+        smush.process(arg, recursive)
+
+    if not quiet:
+        smush.stats()
+
+
+def usage():
+    print """Usage: """ + sys.argv[0] + """ [OPTIONS] DIRECTORIES...
+
+Losslessly optimises image files in DIRECTORIES - this saves bandwidth when
+displaying them on the web.
+WARNING: Existing images in DIRECTORIES will be OVERWRITTEN with optimised
+         versions.
+
+  -h, --help         Display this help message and exit
+
+OPTIONS are any of:
+  -r, --recursive    Recurse through given directories optimising images
+  -q, --quiet        Don't display optimisation statistics at the end
+"""
+
+
 if __name__ == "__main__":
-    optimiser = OptimisePNG()
-    optimiser.set_input('/home/al/temp/temp-images/todo/logo.png')
-    optimiser.optimise()
+    main()
 
-    optimiser = OptimiseJPG()
-    optimiser.set_input('/home/al/temp/temp-images/todo/riding.jpg')
-    optimiser.optimise()
-
-    optimiser = OptimiseGIF()
-    optimiser.set_input('/home/al/temp/temp-images/todo/matrix.gif')
-    optimiser.optimise()
-
-    optimiser = OptimiseGIF()
-    optimiser.set_input('/home/al/temp/temp-images/todo/fish.gif')
-    optimiser.optimise()
+#    optimiser = OptimisePNG()
+#    optimiser.set_input('/home/al/temp/temp-images/todo/logo.png')
+#    optimiser.optimise()
+#
+#    optimiser = OptimiseJPG()
+#    optimiser.set_input('/home/al/temp/temp-images/todo/riding.jpg')
+#    optimiser.optimise()
+#
+#    optimiser = OptimiseGIF()
+#    optimiser.set_input('/home/al/temp/temp-images/todo/matrix.gif')
+#    optimiser.optimise()
+#
+#    optimiser = OptimiseGIF()
+#    optimiser.set_input('/home/al/temp/temp-images/todo/fish.gif')
+#    optimiser.optimise()
